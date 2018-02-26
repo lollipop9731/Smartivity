5c5dcd0f19a5bfb99e6e92cb8ca73c667bc3f969
Lorenz <lorenzhelle@web.de>
2018-02-26 19:27:08 +0100

    Initial commit

---
+++ app/src/main/java/com/example/lorenz/activitytogo/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.lorenz.activitytogo;
+
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView spielen,buch,stern,settings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Actionbar ausblenden
+        getSupportActionBar().hide();
+
+        //Zuweisungen
+        spielen = (TextView)findViewById(R.id.spielen_textview);
+        buch = (TextView)findViewById(R.id.bücher_text);
+        stern = (TextView)findViewById(R.id.star_textview);
+        settings = (TextView)findViewById(R.id.settings_text);
+
+
+        Typeface schrift = Schriftarttypeface();
+        spielen.setTypeface(schrift);
+        buch.setTypeface(schrift);
+        stern.setTypeface(schrift);
+        settings.setTypeface(schrift);
+
+
+
+
+
+    }
+
+    public Typeface Schriftarttypeface(){
+        AssetManager assetManager = getApplicationContext().getAssets();
+        Typeface typeface = Typeface.createFromAsset(assetManager,"fonts/itckrist.ttf");
+        return typeface;
+    }
+
+    /**
+     * Wird aufgerufen, wenn auf das Spielen geklickt wird.
+     * @param view
+     */
+    public void spielenclick(android.view.View view){
+
+        Toast.makeText(getApplicationContext(),"Jetzt startet Spielen",Toast.LENGTH_LONG).show();
+    }
+
+    public void settingsclick(android.view.View view){
+
+        Toast.makeText(getApplicationContext(),"Jetzt starten Einstellungen",Toast.LENGTH_LONG).show();
+    }
+    public void buchclick(android.view.View view){
+
+        Toast.makeText(getApplicationContext(),"So gehts!",Toast.LENGTH_LONG).show();
+    }
+
+    public void starsclick(android.view.View view){
+
+        Toast.makeText(getApplicationContext(),"Mehr Wörter",Toast.LENGTH_LONG).show();
+    }
+
+
+}
